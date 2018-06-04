d00e507ce0ceb26c0323f627be903d54856223eb
Денис Пышкин <avatar48@yandex.ru>
2018-06-04 16:54:38 +0300

    Добавил описание в readme

--- init.rb
+++ init.rb
@@ -1,18 +1,11 @@
 Redmine::Plugin.register :copy_users do
   name 'Copy Users plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'avatar'
+  description 'This is a plugin for Redmine to copy user with roles'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
-  # Rails.application.config.to_prepare do
-  #   UsersController.send(:include, UsersControllerPatch)
-  #   end
+  url 'https://github.com/avatar48/redmine_copy_user'
+  author_url 'https://github.com/avatar48'
 
-    #require 'user'
-  # Rails.configuration.to_prepare do
-  #   UsersController.send(:include, UsersControllerPatch::InstanceMethods)
-  # end
   Rails.configuration.to_prepare do
     require_dependency File.join( File.dirname(File.realpath(__FILE__)), 'lib', 'users_controller_patch' )
     require_dependency File.join( File.dirname(File.realpath(__FILE__)), 'lib', 'users_hook_listener' )
